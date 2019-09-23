95fbafb34779ecf48bc6a0346bd3f22d5d1bd22f
Radiscipio <robertdiscipio@gmail.com>
2019-09-23 17:07:32 -0400

    Added data structure to pull from

---
+++ brads-recipes/src/data.js
@@ -0,0 +1,85 @@
+let data = [
+  {
+    name: 'Blueberry Pancakes',
+    image: '',
+    time: '',
+    feeds: '',
+    price: '',
+    directions: [ ],
+    ingredients: [ ]
+  },
+  {
+    name: 'Chicken Parmesan',
+    image: '',
+    time: '',
+    feeds: '',
+    price: '',
+    directions: '',
+    ingredients: []
+  },
+  {
+    name: 'Seafood Cioppino',
+    image: '',
+    time: '',
+    feeds: '',
+    price: '',
+    directions: '',
+    ingredients: []
+  },
+  {
+    name: 'Ajiaco',
+    image: '',
+    time: '',
+    feeds: '',
+    price: '',
+    directions: '',
+    ingredients: []
+  },
+  {
+    name: 'Chicken Quessadilla',
+    image: '',
+    time: '',
+    feeds: '',
+    price: '',
+    directions: '',
+    ingredients: []
+  },
+  {
+    name: '',
+    image: '',
+    time: '',
+    feeds: '',
+    price: '',
+    directions: '',
+    ingredients: []
+  },
+  {
+    name: '',
+    image: '',
+    time: '',
+    feeds: '',
+    price: '',
+    directions: '',
+    ingredients: []
+  },
+  {
+    name: '',
+    image: '',
+    time: '',
+    feeds: '',
+    price: '',
+    directions: '',
+    ingredients: []
+  },
+  {
+    name: '',
+    image: '',
+    time: '',
+    feeds: '',
+    price: '',
+    directions: '',
+    ingredients: []
+  }
+]
+
+export default data
