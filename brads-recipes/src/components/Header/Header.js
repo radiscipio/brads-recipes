aef08c9f39653805a2a7f16c0b8ed79d0b5b7cdb
eloisebarrow <eloisebarrow@gmail.com>
2019-09-24 11:25:43 -0400

    header skeleton layout complete

--- brads-recipes/src/components/Header/Header.js
+++ brads-recipes/src/components/Header/Header.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './header.css';
 
 function Header() {
   return (
-    <div>
-      <h1>This is Header</h1>
+    <div className="header">
+      <div className="logo">
+        <div className="sublogo">
+          <a href="#">LOGO</a>
+          <h1>ReciPlease</h1>
+        </div>
+
+        <div className="sublogo">
+          <a href="#">PROFILE PIC</a>
+          <a href="#">CART</a>
+        </div>
+
+      </div>
+      <div className="nav">
+        <a href="#">Home</a>
+        <a href="#">My Meals</a>
+        <a href="#">About</a>
+      </div>
     </div>
   )
 }
