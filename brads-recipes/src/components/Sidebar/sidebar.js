e4a3494fa356c1d023995a236e503069bdd016b3
eloisebarrow <eloisebarrow@gmail.com>
2019-09-24 15:24:47 -0400

    sidebar additions

--- brads-recipes/src/components/Sidebar/sidebar.js
+++ brads-recipes/src/components/Sidebar/sidebar.js
@@ -4,15 +4,27 @@ import './sidebar.css'
 function Sidebar() {
   return (
     <div className="sidebar-container">
-      <div>
-        <select name="filter">
-          <option>Filter: </option>
+      <div className="sort-by">
+        <select className="dropdown" name="sort-by">
+          <option>Sort By:</option>
+          <option>No. of ingredients (low to high)</option>
+          <option>No. of ingredients (high to low)</option>
+          <option>Cook time (low to high)</option>
+          <option>Cook time (high to low)</option>
+          <option>Price (low to high)</option>
+          <option>Price (high to low)</option>
         </select>
       </div>
-      <div>
-      <select name="sort-by">
-        <option>Sort By:</option>
-      </select>
+
+      <div className="filter">
+        <select className="dropdown" name="filter">
+          <option>Filter:</option>
+          <option>Breakfast</option>
+          <option>Lunch</option>
+          <option>Dinner</option>
+          <option>Dietary Restrictions</option>
+          <option>Allergies</option>
+        </select>
       </div>
     </div>
   )
