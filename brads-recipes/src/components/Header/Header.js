8c7c609d697b8c1b7ead2f813b0bb73c897b2f44
eloisebarrow <eloisebarrow@gmail.com>
2019-09-24 16:14:36 -0400

    responsive header mostly finished

--- brads-recipes/src/components/Header/Header.js
+++ brads-recipes/src/components/Header/Header.js
@@ -5,7 +5,12 @@ function Header() {
   return (
     <div className="header">
       <div className="logo">
-        <div className="sublogo">
+        <div className="hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="sublogo sublogo-1">
           <a href="#">
             <img
               className="reciplease-logo"
@@ -25,7 +30,7 @@ function Header() {
           <a href="#">
             <img
               className="shopping-cart"
-              src="https://res.cloudinary.com/radiscipio/image/upload/v1569344616/e0jwuoi6lo61uqmillj2.png"
+              src="https://res.cloudinary.com/eloise/image/upload/v1569355821/noun_cart_2289817_y0hr4a.png"
             />
           </a>
         </div>
